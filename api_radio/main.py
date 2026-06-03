4bb977fc6bc1615cb9387effc556957ece2e9f7b
msbs <marie.soubies.auditeur@lecnam.net>
2026-06-03 12:49:10 +0200

    Mise en place des indicateurs de suivi (KPI) — niveau logging

--- api_radio/main.py
+++ api_radio/main.py
@@ -1,8 +1,18 @@
+import time
+
 from fastapi import FastAPI, UploadFile, File
 from api_radio.schemas import PredictionResponse
 from api_radio.singleton_model import ModelSingleton
 from api_radio.preprocessing import preprocess_image
 import torch
+import logging
+
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s | %(levelname)s | %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
+
+logger = logging.getLogger("api_radio")
 
 app = FastAPI(
     title="API Détection Anomalies Radiologiques",
@@ -13,6 +23,7 @@ app = FastAPI(
 @app.on_event("startup")
 async def startup():
     ModelSingleton.get_instance()
+    logger.info("Modèle chargé et API prête")
 
 @app.get("/health")
 def health():
@@ -20,6 +31,8 @@ def health():
 
 @app.post("/predict", response_model=PredictionResponse)
 async def predict(image: UploadFile = File(...)):
+    start = time.time()
+
     image_bytes = await image.read()
     tensor = preprocess_image(image_bytes=image_bytes)
     feature_extractor, head = ModelSingleton.get_instance()
@@ -30,6 +43,25 @@ async def predict(image: UploadFile = File(...)):
         probabilite = torch.sigmoid(output).item()
 
     label = "malade" if probabilite > 0.5 else "sain"
+    latence = round(time.time() - start,4)
+
+    # ================== KPI modèle : score, label) ==========================#
+    logger.info(
+        f"PREDICT | label={label} | "
+        f"probabilite={probabilite:.4f} | "
+        f"latence={latence} | "
+        f"fichier={image.filename}"
+    )
+
+    # =================== ALerte automatique : LANTENCE + AMBIGUITÉ =================================#
+    if latence > 2.0:
+        logger.warning(f"ALERTE LATENCE | {latence}s > seuil 2.0s")
+
+    if 0.4 < probabilite < 0.6:
+        logger.warning(
+            f"ALERTE AMBIGUÏTÉ | probabilite={round(probabilite, 4)} "
+            f"— supervision humaine requise (radiologue)"
+        )
 
     return PredictionResponse(
         label=label,
